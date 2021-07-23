a6bdae9c5ab20eff62572bf22cdf55d1523e9f0f
gadaharon <gadaharon91@gmail.com>
2021-07-23 11:27:05 +0300

    add main index to run the chip8 class

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import { Chip8 } from "./Chip8";
+
+const chip8 = new Chip8();
+chip8.registers.stackPush(1);
+chip8.registers.stackPush(2);
+chip8.registers.stackPush(3);
+let result;
+result = chip8.registers.stackPop();
+console.log(result);
+result = chip8.registers.stackPop();
+console.log(result);
+result = chip8.registers.stackPop();
+console.log(result);
+
+
