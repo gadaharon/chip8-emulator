daa926b60c0b5e74d1e6477af0bc639267801bb4
gadaharon <gadaharon91@gmail.com>
2021-07-23 11:26:21 +0300

    add Display class

---
+++ src/Display.js
@@ -0,0 +1,52 @@
+import {
+  BG_COLOR,
+  COLOR,
+  DISPLAY_HEIGHT,
+  DISPLAY_MULTIPLAY,
+  DISPLAY_WIDTH,
+} from '../src/constants/displayConstants';
+export class Display {
+  constructor() {
+    console.log('Create new Display');
+    this.screen = document.querySelector('canvas');
+    this.screen.width = DISPLAY_WIDTH * DISPLAY_MULTIPLAY;
+    this.screen.height = DISPLAY_HEIGHT * DISPLAY_MULTIPLAY;
+    this.context = this.screen.getContext('2d');
+    this.context.fillStyle = BG_COLOR;
+    this.frameBuffer = [];
+    this.context.fillRect(0, 0, this.screen.width, this.screen.height);
+    this.reset();
+  }
+  reset() {
+    for (let i = 0; i < DISPLAY_HEIGHT; i++) {
+      this.frameBuffer.push([]);
+      for (let j = 0; j < DISPLAY_WIDTH; j++) {
+        this.frameBuffer[i].push(1);
+      }
+    }
+    this.context.fillRect(0, 0, this.screen.width, this.screen.height);
+    this.drawBuffer();
+  }
+  drawBuffer() {
+    for (let h = 0; h < DISPLAY_HEIGHT; h++) {
+      this.frameBuffer.push([]);
+      for (let w = 0; w < DISPLAY_WIDTH; w++) {
+        this.drawPixel(h, w, this.frameBuffer[h][w]);
+      }
+    }
+  }
+
+  drawPixel(h, w, value) {
+    if (value) {
+      this.context.fillStyle = COLOR;
+    } else {
+      this.context.fillStyle = BG_COLOR;
+    }
+    this.context.fillRect(
+      w * DISPLAY_MULTIPLAY,
+      h * DISPLAY_MULTIPLAY,
+      DISPLAY_MULTIPLAY,
+      DISPLAY_MULTIPLAY
+    );
+  }
+}
