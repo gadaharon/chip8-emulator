65961a19ed432365b90ecd65f059d5b00a8e4921
gadaharon <gadaharon91@gmail.com>
2021-08-03 20:26:12 +0300

    draw sprites

--- src/index.js
+++ src/index.js
@@ -2,12 +2,11 @@ import { Chip8 } from './Chip8';
 
 const chip8 = new Chip8();
 async function runChip8() {
-  while (true) {
-    const hasKeydown = chip8.keyboard.hasKeydown();
-    const isKeydown = chip8.keyboard.isKeyDown(1);
-    console.log({ hasKeydown, isKeydown });
-    await chip8.sleep();
-  }
+  chip8.display.drawSprite(0, 0, 0, 5)
+  chip8.display.drawSprite(0, 5, 5, 5)
+  chip8.display.drawSprite(0, 10, 10, 5)
+  chip8.display.drawSprite(0, 15, 15, 5)
+  chip8.display.drawSprite(0, 20, 20, 5)
 }
 
-// runChip8();
+runChip8();
